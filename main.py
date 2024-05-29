3df71971410a7ccc0e9a6693162e3749a8e2cc3d
Rizhiker <eduard789rf@gmail.com>
2024-05-29 19:38:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,31 @@ win_height = 500
 window = display.set_mode((winf_width, win_height))
 window.fill(back)
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y,size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y        
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        key_pressed = key.get_pressed()
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+    def update_r(self):
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        
+player = Player('racket.png', 5, win_height-100,80,100, 10)
+myachik = Player('tenis_ball.png', 0, win_height-100,40,100,10)
+
 game = True
 finish = False
 clock = time.Clock()
