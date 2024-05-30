a37c259af4fe22403d22b89e7ae73e742872af2a
Rizhiker <eduard789rf@gmail.com>
2024-05-30 15:13:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,13 +22,28 @@ class Player(GameSprite):
     def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
+            self.rect.y += self.speed
+        if keys[K_s] and self.rect.y > 5:
             self.rect.y -= self.speed
     def update_r(self):
         if keys[K_UP] and self.rect.y > 5:
+            self.rect.y += self.speed
+        if keys[K_DOWN] and self.rect.y > 5:
             self.rect.y -= self.speed
         
-player = Player('racket.png', 5, win_height-100,80,100, 10)
-myachik = Player('tenis_ball.png', 0, win_height-100,40,100,10)
+racket1 = Player('racket (1).png', 30,200,4,50,150)
+racket2 = Player('racket (1).png', 520,200,4,50,150)
+ball = GameSprite('tenis_ball (1).png', 200,200,4,50,50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0 ,0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
+
 
 game = True
 finish = False
@@ -39,6 +54,32 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+        if  finish != True:
+            ball.rect.x += speed_x
+            ball.rect.y += speed_y
     
+    if finish != True:
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > win_height - 50 or ball_rect.y < 0:
+            speed_y *= -1
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            spped_y *= -1
+
+        if ball.rect.x > win.width:
+            finish = True
+            window.blit(lose2, (200,200))
+
+        if ball.rect.x < win.width:
+            finish = True
+            window.blit(lose1, (200,200))
+
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
     display.update()
     clock.tick(FPS)
